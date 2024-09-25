1420f9e5e060257c46cb579dd7fa5e2d398f4360
night0721 <night@night0721.xyz>
2024-09-25 18:01:42 +0100

    Make show notification write error to log file instead of printing it

--- src/lib/notification.c
+++ src/lib/notification.c
@@ -6,10 +6,10 @@ void send_notification(uint8_t *author, uint8_t *content)
     NotifyNotification *notification = notify_notification_new((char *) author,
             (char *) content, "dialog-information");
     if (notification == NULL) {
-        error(0, "Cannot create notification");
+        write_log(LOG_ERROR, "Cannot create notification");
     }
     if (!notify_notification_show(notification, NULL)) {
-        error(0, "Cannot show notifcation");
+        write_log(LOG_ERROR, "Cannot show notifcation");
     }
     g_object_unref(G_OBJECT(notification));
 }
